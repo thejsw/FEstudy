643bec52dc970bbc8201ead5edcae959bc70b532
JSW <jswtncapetnc@gmail.com>
2022-04-11 19:23:23 +0900

    project toss.im clear

--- project/toss.im/main.js
+++
@@ -1,6 +0,0 @@
-const toggleBtn = document.querySelector(button);
-const navContent = document.querySelector('.p-navbar--content-list');
-
-toggleBtn.addEventListener('click', () => {
-    navContent.classList.toggle('active');
-});
