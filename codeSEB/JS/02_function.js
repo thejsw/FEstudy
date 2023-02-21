084692b7ae48df98833011f031d630ad814ceee4
thejsw <jswtncapetnc@gmail.com>
2023-02-21 20:38:54 +0900

    FEstduy_02_function.js

---
+++ codeSEB/JS/02_function.js
@@ -0,0 +1,10 @@
+// 실습) 아래 함수들을 각각 호출하고 결과를 확인하세요.
+function declared() {
+    console.log('함수선언문');
+  }
+  
+  let expression = function () {
+    console.log('함수표현식');
+  };
+  
+  // 여기에 코드를 작성하세요.
